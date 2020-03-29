c47497a483dd76e32195dbb4cc852cceb7783348
MarcoFavorito <marco.favorito@gmail.com>
2020-03-29 10:25:53 +0200

    simplify _ci-configure.sh and build-dev.sh

--- scripts/_ci-configure.sh
+++ scripts/_ci-configure.sh
@@ -1,15 +1,9 @@
 #!/bin/bash -e
+# exit script if a command fails
 set -e
-# TOFIX exit script if a command fails
 
-if [ $# -ne 1 ]; then
-echo -e "\033[0;31m[!] Usage: ./scripts/build-cmake-dev.sh <SRC_ROOT> \033[0m"
-exit 1
-fi
-
-SRC_ROOT="$1"
 BUILD_DIR=build
-cd "${SRC_ROOT}" && mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR} && rm -rf ./*
+mkdir -p ${BUILD_DIR} && cd ${BUILD_DIR} && rm -rf ./*
 
 # Configure compiler build with coverage flas
-cmake "${SRC_ROOT}" -DENABLE_COVERAGE=ON
+cmake .. -DENABLE_COVERAGE=ON
