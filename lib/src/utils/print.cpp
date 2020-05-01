beab56d9a15f7b447d9abd826e9a338ed0c18933
Francesco Fuggitti <francesco.fuggitti@gmail.com>
2020-05-01 08:56:05 +0200

    small fix on LDLfBox print

--- lib/src/utils/print.cpp
+++ lib/src/utils/print.cpp
@@ -66,7 +66,7 @@ void StrPrinter::visit(const LDLfDiamond &x) {
 
 void StrPrinter::visit(const LDLfBox &x) {
   std::ostringstream s;
-  s << "<" << apply(*x.get_regex()) << ">(" << apply(*x.get_formula()) << ")";
+  s << "[" << apply(*x.get_regex()) << "](" << apply(*x.get_formula()) << ")";
   result = s.str();
 }
 
